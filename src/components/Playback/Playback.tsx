bf41213f3c77233b8afa1e765cd2d00cb4d57b2e
Daniel Lineberger <ace017@gmail.com>
2021-10-13 01:03:40 -0600

    Add simple recording

--- src/components/Playback/Playback.tsx
+++ src/components/Playback/Playback.tsx
@@ -2,6 +2,31 @@ import { Circle, Play } from 'react-feather';
 import './Playback.css';
 
 function Playback() {
+  async function beginRecording() {
+    console.log('begin recording!');
+
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+
+    const audioChunks: any[] = [];
+
+    mediaRecorder.addEventListener('dataavailable', event => {
+      audioChunks.push(event.data);
+    });
+
+    mediaRecorder.addEventListener('stop', () => {
+      const audioBlob = new Blob(audioChunks);
+      const audioUrl = URL.createObjectURL(audioBlob);
+      const audio = new Audio(audioUrl);
+      audio.play();
+    });
+
+    setTimeout(() => {
+      mediaRecorder.stop();
+    }, 6000);
+  }
+
   return (
     <div className="playback">
       <div className="controls">
@@ -9,7 +34,7 @@ function Playback() {
           <Play color={"green"} fill={"green"} size={30} />
         </div>
 
-        <div className="control">
+        <div className="control" onClick={() => beginRecording()}>
           <Circle color={"red"} fill={"red"} size={30} />
         </div>
       </div>
